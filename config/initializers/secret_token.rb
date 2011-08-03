fd613da7ddee4f2c502b3bb1fe8fbb2d36b316c2
U-UD2\user <user@ud2.(none)>
2011-08-03 19:09:14 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = 'deb71bb902c3830246d2ac854356803763e9b964f3ad48c433a040404dc3ebfb1e2f4af09bb19f6fe043a45a4f5f713411d2f59d251794c25100f4f7dba4695b'
